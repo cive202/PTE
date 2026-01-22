02a5313ea985cd6516ba9dacade92e82c5737578
cive202 <civecodes@gmail.com>
2026-01-22 12:24:16 +0545

    pseudo2text

---
+++ read_aloud/pseudo_voice2text.py
@@ -0,0 +1,1046 @@
+from typing import Any, Dict, List 
+
+WORD_PESUDO =[
+  {
+    "type": "word",
+    "start": 1.28,
+    "end": 2.16,
+    "value": "Health"
+  },
+  {
+    "type": "word",
+    "start": 2.48,
+    "end": 3.2800000000000002,
+    "value": "Maintaining"
+  },
+  {
+    "type": "word",
+    "start": 3.2800000000000002,
+    "end": 3.7600000000000002,
+    "value": "physical"
+  },
+  {
+    "type": "word",
+    "start": 3.7600000000000002,
+    "end": 4.16,
+    "value": "health"
+  },
+  {
+    "type": "word",
+    "start": 4.16,
+    "end": 5.04,
+    "value": "requires"
+  },
+  {
+    "type": "word",
+    "start": 5.04,
+    "end": 5.6000000000000005,
+    "value": "regular"
+  },
+  {
+    "type": "word",
+    "start": 5.6000000000000005,
+    "end": 6.32,
+    "value": "exercise,"
+  },
+  {
+    "type": "word",
+    "start": 6.48,
+    "end": 7.04,
+    "value": "balanced"
+  },
+  {
+    "type": "word",
+    "start": 7.04,
+    "end": 7.6000000000000005,
+    "value": "nutrition,"
+  },
+  {
+    "type": "word",
+    "start": 7.76,
+    "end": 8.0,
+    "value": "and"
+  },
+  {
+    "type": "word",
+    "start": 8.0,
+    "end": 8.56,
+    "value": "adequate"
+  },
+  {
+    "type": "word",
+    "start": 8.56,
+    "end": 9.040000000000001,
+    "value": "sleep."
+  },
+  {
+    "type": "word",
+    "start": 9.36,
+    "end": 9.92,
+    "value": "Medical"
+  },
+  {
+    "type": "word",
+    "start": 9.92,
+    "end": 10.8,
+    "value": "professionals"
+  },
+  {
+    "type": "word",
+    "start": 10.8,
+    "end": 11.28,
+    "value": "warn"
+  },
+  {
+    "type": "word",
+    "start": 11.44,
+    "end": 11.68,
+    "value": "that"
+  },
+  {
+    "type": "word",
+    "start": 11.68,
+    "end": 12.48,
+    "value": "sedentary"
+  },
+  {
+    "type": "word",
+    "start": 12.48,
+    "end": 13.200000000000001,
+    "value": "lifestyles"
+  },
+  {
+    "type": "word",
+    "start": 13.200000000000001,
+    "end": 13.68,
+    "value": "increase"
+  },
+  {
+    "type": "word",
+    "start": 13.68,
+    "end": 13.84,
+    "value": "the"
+  },
+  {
+    "type": "word",
+    "start": 13.84,
+    "end": 14.16,
+    "value": "risk"
+  },
+  {
+    "type": "word",
+    "start": 14.16,
+    "end": 14.4,
+    "value": "of"
+  },
+  {
+    "type": "word",
+    "start": 14.4,
+    "end": 14.88,
+    "value": "chronic"
+  },
+  {
+    "type": "word",
+    "start": 14.88,
+    "end": 15.6,
+    "value": "diseases."
+  },
+  {
+    "type": "word",
+    "start": 15.92,
+    "end": 16.48,
+    "value": "Public"
+  },
+  {
+    "type": "word",
+    "start": 16.48,
+    "end": 17.04,
+    "value": "awareness"
+  },
+  {
+    "type": "word",
+    "start": 17.04,
+    "end": 17.2,
+    "value": "and"
+  },
+  {
+    "type": "word",
+    "start": 17.2,
+    "end": 17.84,
+    "value": "prevention"
+  },
+  {
+    "type": "word",
+    "start": 17.84,
+    "end": 18.32,
+    "value": "programs"
+  },
+  {
+    "type": "word",
+    "start": 18.400000000000002,
+    "end": 18.56,
+    "value": "are"
+  },
+  {
+    "type": "word",
+    "start": 18.56,
+    "end": 19.28,
+    "value": "essential."
+  }
+]
+CHAR_PESUDO =[
+  {
+    "type": "char",
+    "start": 1.28,
+    "end": 1.52,
+    "value": [
+      "He"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 1.52,
+    "end": 1.68,
+    "value": [
+      "al"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 1.84,
+    "end": 2.16,
+    "value": [
+      "th"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 2.48,
+    "end": 2.64,
+    "value": [
+      "M"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 2.64,
+    "end": 2.8000000000000003,
+    "value": [
+      "ain"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 2.8000000000000003,
+    "end": 2.96,
+    "value": [
+      "t"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 2.96,
+    "end": 3.12,
+    "value": [
+      "ain"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 3.12,
+    "end": 3.2800000000000002,
+    "value": [
+      "ing"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 3.2800000000000002,
+    "end": 3.36,
+    "value": [
+      "ph"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 3.36,
+    "end": 3.52,
+    "value": [
+      "ys"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 3.52,
+    "end": 3.7600000000000002,
+    "value": [
+      "ical"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 3.7600000000000002,
+    "end": 3.84,
+    "value": [
+      "he"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 3.84,
+    "end": 4.0,
+    "value": [
+      "al"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 4.0,
+    "end": 4.16,
+    "value": [
+      "th"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 4.16,
+    "end": 4.4,
+    "value": [
+      "re"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 4.4,
+    "end": 4.5600000000000005,
+    "value": [
+      "qu"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 4.5600000000000005,
+    "end": 4.8,
+    "value": [
+      "i"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 4.8,
+    "end": 5.04,
+    "value": [
+      "res"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 5.04,
+    "end": 5.36,
+    "value": [
+      "reg"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 5.36,
+    "end": 5.6000000000000005,
+    "value": [
+      "ular"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 5.6000000000000005,
+    "end": 5.84,
+    "value": [
+      "ex"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 5.84,
+    "end": 6.0,
+    "value": [
+      "er"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 6.0,
+    "end": 6.16,
+    "value": [
+      "c"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 6.16,
+    "end": 6.32,
+    "value": [
+      "ise"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 6.32,
+    "end": 6.32,
+    "value": [
+      ","
+    ]
+  },
+  {
+    "type": "char",
+    "start": 6.48,
+    "end": 6.5600000000000005,
+    "value": [
+      "b"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 6.5600000000000005,
+    "end": 6.72,
+    "value": [
+      "al"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 6.72,
+    "end": 6.8,
+    "value": [
+      "an"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 6.8,
+    "end": 6.96,
+    "value": [
+      "c"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 6.96,
+    "end": 7.04,
+    "value": [
+      "ed"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 7.04,
+    "end": 7.12,
+    "value": [
+      "n"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 7.12,
+    "end": 7.28,
+    "value": [
+      "ut"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 7.28,
+    "end": 7.44,
+    "value": [
+      "r"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 7.44,
+    "end": 7.6000000000000005,
+    "value": [
+      "ition"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 7.6000000000000005,
+    "end": 7.6000000000000005,
+    "value": [
+      ","
+    ]
+  },
+  {
+    "type": "char",
+    "start": 7.76,
+    "end": 8.0,
+    "value": [
+      "and"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 8.0,
+    "end": 8.16,
+    "value": [
+      "ad"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 8.16,
+    "end": 8.24,
+    "value": [
+      "e"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 8.24,
+    "end": 8.4,
+    "value": [
+      "qu"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 8.4,
+    "end": 8.56,
+    "value": [
+      "ate"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 8.56,
+    "end": 8.64,
+    "value": [
+      "s"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 8.64,
+    "end": 8.88,
+    "value": [
+      "le"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 8.88,
+    "end": 9.040000000000001,
+    "value": [
+      "ep"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 9.040000000000001,
+    "end": 9.040000000000001,
+    "value": [
+      "."
+    ]
+  },
+  {
+    "type": "char",
+    "start": 9.36,
+    "end": 9.52,
+    "value": [
+      "M"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 9.52,
+    "end": 9.68,
+    "value": [
+      "ed"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 9.68,
+    "end": 9.92,
+    "value": [
+      "ical"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 9.92,
+    "end": 10.16,
+    "value": [
+      "pro"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 10.16,
+    "end": 10.24,
+    "value": [
+      "f"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 10.24,
+    "end": 10.4,
+    "value": [
+      "ess"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 10.4,
+    "end": 10.56,
+    "value": [
+      "ion"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 10.56,
+    "end": 10.8,
+    "value": [
+      "als"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 10.8,
+    "end": 10.96,
+    "value": [
+      "w"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 10.96,
+    "end": 11.120000000000001,
+    "value": [
+      "ar"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 11.120000000000001,
+    "end": 11.28,
+    "value": [
+      "n"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 11.44,
+    "end": 11.68,
+    "value": [
+      "that"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 11.68,
+    "end": 11.84,
+    "value": [
+      "s"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 11.84,
+    "end": 12.08,
+    "value": [
+      "ed"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 12.08,
+    "end": 12.32,
+    "value": [
+      "ent"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 12.32,
+    "end": 12.48,
+    "value": [
+      "ary"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 12.48,
+    "end": 12.56,
+    "value": [
+      "li"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 12.56,
+    "end": 12.72,
+    "value": [
+      "f"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 12.72,
+    "end": 12.88,
+    "value": [
+      "est"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 12.88,
+    "end": 13.040000000000001,
+    "value": [
+      "y"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 13.040000000000001,
+    "end": 13.200000000000001,
+    "value": [
+      "les"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 13.200000000000001,
+    "end": 13.36,
+    "value": [
+      "inc"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 13.36,
+    "end": 13.52,
+    "value": [
+      "re"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 13.52,
+    "end": 13.68,
+    "value": [
+      "ase"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 13.68,
+    "end": 13.84,
+    "value": [
+      "the"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 13.84,
+    "end": 13.92,
+    "value": [
+      "r"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 13.92,
+    "end": 14.08,
+    "value": [
+      "is"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 14.08,
+    "end": 14.16,
+    "value": [
+      "k"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 14.16,
+    "end": 14.4,
+    "value": [
+      "of"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 14.4,
+    "end": 14.48,
+    "value": [
+      "ch"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 14.48,
+    "end": 14.64,
+    "value": [
+      "r"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 14.64,
+    "end": 14.72,
+    "value": [
+      "on"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 14.72,
+    "end": 14.88,
+    "value": [
+      "ic"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 14.88,
+    "end": 15.120000000000001,
+    "value": [
+      "dis"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 15.120000000000001,
+    "end": 15.280000000000001,
+    "value": [
+      "e"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 15.280000000000001,
+    "end": 15.44,
+    "value": [
+      "as"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 15.44,
+    "end": 15.6,
+    "value": [
+      "es"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 15.6,
+    "end": 15.6,
+    "value": [
+      "."
+    ]
+  },
+  {
+    "type": "char",
+    "start": 15.92,
+    "end": 16.080000000000002,
+    "value": [
+      "P"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 16.080000000000002,
+    "end": 16.240000000000002,
+    "value": [
+      "ub"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 16.240000000000002,
+    "end": 16.48,
+    "value": [
+      "lic"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 16.48,
+    "end": 16.56,
+    "value": [
+      "a"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 16.56,
+    "end": 16.72,
+    "value": [
+      "w"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 16.72,
+    "end": 16.8,
+    "value": [
+      "are"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 16.8,
+    "end": 17.04,
+    "value": [
+      "ness"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 17.04,
+    "end": 17.2,
+    "value": [
+      "and"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 17.2,
+    "end": 17.36,
+    "value": [
+      "pre"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 17.36,
+    "end": 17.52,
+    "value": [
+      "v"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 17.52,
+    "end": 17.84,
+    "value": [
+      "ention"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 17.84,
+    "end": 17.92,
+    "value": [
+      "pro"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 17.92,
+    "end": 18.080000000000002,
+    "value": [
+      "gr"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 18.080000000000002,
+    "end": 18.16,
+    "value": [
+      "am"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 18.16,
+    "end": 18.32,
+    "value": [
+      "s"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 18.400000000000002,
+    "end": 18.56,
+    "value": [
+      "are"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 18.56,
+    "end": 18.72,
+    "value": [
+      ""
+    ]
+  },
+  {
+    "type": "char",
+    "start": 18.72,
+    "end": 18.96,
+    "value": [
+      "ess"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 18.96,
+    "end": 19.12,
+    "value": [
+      "ent"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 19.12,
+    "end": 19.28,
+    "value": [
+      "ial"
+    ]
+  },
+  {
+    "type": "char",
+    "start": 19.28,
+    "end": 19.28,
+    "value": [
+      "."
+    ]
+  }
+]
+SEGMENT_PESUDO =[
+  {
+    "type": "segment",
+    "start": 1.28,
+    "end": 9.040000000000001,
+    "value": "Health Maintaining physical health requires regular exercise, balanced nutrition, and adequate sleep."
+  },
+  {
+    "type": "segment",
+    "start": 9.36,
+    "end": 15.6,
+    "value": "Medical professionals warn that sedentary lifestyles increase the risk of chronic diseases."
+  },
+  {
+    "type": "segment",
+    "start": 15.92,
+    "end": 19.28,
+    "value": "Public awareness and prevention programs are essential."
+  }
+]
+
+def voice2text_word() -> List[Dict[str,Any]]:
+    return WORD_PESUDO
+
+def voice2text_char() -> List[Dict[str,Any]]:
+    return CHAR_PESUDO
+
+def voice2text_segment() ->List[Dict[str,Any]]:
+    return SEGMENT_PESUDO
+
